a757465b3ddf916814f2a6626868e70d3689d6d6
RaoVrn <prakash.varun.0305@gmail.com>
2025-05-24 22:48:18 +0530

    ⛰️ Add centered pyramid star pattern using both methods

--- cppBasics/07_patterns/44_centeredPyramidStars.cpp
+++ cppBasics/07_patterns/44_centeredPyramidStars.cpp
@@ -15,3 +15,72 @@ An integer `n` for number of rows.
 ➤ Method 1: Use nested loops in `main()`.
 ➤ Method 2: Use `printCenteredPyramid(int n)` function.
 */
+
+
+// -------------------------
+// Method 1: Logic inside main function
+// -------------------------
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cout << "Enter the value of n: ";
+	cin >> n;
+
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n-i-1; j++) {
+			cout << " ";
+		}
+	
+		for(int j=0; j<2*i+1; j++) {
+			cout << "*";
+		}
+
+		for(int j=0; j<n-i-1; j++) {
+			cout << " ";
+		}
+		
+		cout << endl;
+	}
+
+	return 0;
+}
+
+
+
+// -------------------------
+// Method 2: Using a function
+// -------------------------
+
+#include<iostream>
+using namespace std;
+
+void printCenteredPyramid(int n) {
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n-i-1; j++) {
+			cout << " ";
+		}
+	
+		for(int j=0; j<2*i+1; j++) {
+			cout << "*";
+		}
+
+		for(int j=0; j<n-i-1; j++) {
+			cout << " ";
+		}
+		
+		cout << endl;
+	}
+}
+
+int main() {
+	int n;
+	cout << "Enter the value of n: ";
+	cin >> n;
+
+	printCenteredPyramid(n);
+
+	return 0;
+}
